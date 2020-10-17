334e4f8867a864b844a3a85d50acaafc3c1f81cc
Dilip <dilipsinghdangwal@gmail.com>
2020-10-17 22:47:21 +0530

    finished the homeLayout design

---
+++ src/components/directory/Directory.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import MenuItem from "../menu-item/MenuItem";
+import "./Directory.scss";
+
+function Directory() {
+  const sections = [
+    {
+      title: "HATS",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      id: 1,
+      linkUrl: "shop/hats",
+    },
+    {
+      title: "JACKETS",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      id: 2,
+      linkUrl: "shop/jackets",
+    },
+    {
+      title: "SNEAKERS",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      id: 3,
+      linkUrl: "shop/sneakers",
+    },
+    {
+      title: "WOMEN",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      size: "large",
+      id: 4,
+      linkUrl: "shop/womens",
+    },
+    {
+      title: "MEN",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      size: "large",
+      id: 5,
+      linkUrl: "shop/mens",
+    },
+  ];
+  return (
+    <div className="directory-menu">
+      {sections.map((section) => (
+        <MenuItem
+          key={section.id}
+          title={section.title}
+          imageUrl={section.imageUrl}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Directory;
